9810a0a7d9e294c656028058c7bb9b3f48a6fa77
Enrico Gobbi <enrico.gobbi.itiscesena@gmail.com>
2019-03-21 16:17:54 +0100

    Inizializzazione file

---
+++ FTP_Server/FTP_Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FTP_Server
+{
+    class Program
+    {
+        static IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("10.13.0.23"), 60100);
+        static TcpListener srv = new TcpListener(ipServer);
+
+        static void Listening()
+        {
+            while (true)
+            {
+                srv.Start();
+                srv.AcceptTcpClient();
+
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(Listening);
+            th.Start();
+        }
+    }
+}
