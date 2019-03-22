5de544b8201bdfb3c9f3d61c7d36137af3327d23
Enrico Gobbi <enrico.gobbi.itiscesena@gmail.com>
2019-03-22 19:52:06 +0100

    Completamento parziale del server (Non testato).
    In attesa del del completamento del client per testare

--- FTP_Server/FTP_Server/Program.cs
+++ FTP_Server/FTP_Server/Program.cs
@@ -6,28 +6,56 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.IO;
 
 namespace FTP_Server
 {
     class Program
     {
-        static IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("10.13.0.23"), 60100);
+        static IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("192.168.1.10"), 60100);
         static TcpListener srv = new TcpListener(ipServer);
+        static TcpClient client;
+        static string root = @"C:/Users/Administrator/Desktop/ftp";
 
         static void Listening()
         {
+            NetworkStream stream;
+            byte[] buffer = new byte[2048];
+            StreamWriter sw;
+            string[] str;
+            string path;
+
             while (true)
             {
-                srv.Start();
-                srv.AcceptTcpClient();
+                client = srv.AcceptTcpClient();
+                Console.WriteLine("Connesso client");
 
+                //lettura da rete
+                stream = client.GetStream();
+                stream.Read(buffer, 0, buffer.Length);
+
+                //conversione da byte a ascii
+                str = Encoding.ASCII.GetString(buffer).Split(',');
+
+                //ricavo del percorso (dopo la root)
+                path = str[0].Substring(str[0].IndexOf('/'));
                 
+                //scrittura del file
+                sw = new StreamWriter(@root + @"/" + path);
+                sw.Write(str);
+                sw.Flush();
+                sw.Close();
             }
         }
 
         static void Main(string[] args)
         {
             Thread th = new Thread(Listening);
+
+            Console.WriteLine("Start");
+            srv.Start();
+
+            Console.WriteLine("Attesa connessioni...");
             th.Start();
         }
     }
